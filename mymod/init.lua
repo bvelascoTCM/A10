e24099dc295f6e5347db570f6c85bf9adce8921d
AdrianGarciaMartin <77283506+AdrianGarciaMartin@users.noreply.github.com>
2021-11-15 20:37:18 +0100

    paracetamol

--- mymod/init.lua
+++ mymod/init.lua
@@ -1,1 +1,23 @@
-minetest.register_node("mymod: flash_kid", {description = } )
+minetest.register_node("mymod:cubonejo", {
+    description = "My node", 
+    tiles = {"mymod_cubonejo.png"}, --imagen que definira el cubo (la imagen del conejo se repite en cada cara)
+    groups = {cracky = 3} --cracky = tiempo que tarda en romper el cubo
+  })
+minetest.register_craftitem("mymod:cubonejo", {
+    description = "My Item",
+    inventory_image = "mymod_cubonejo.png"
+})
+
+minetest.register_craft({ --revisalo
+    output = "mymod:cubonejo",
+    recipe = {
+      {"default:string", "default:string"},
+      {"default:string", "default:string"},
+      {"default:string", "default:string"}
+    }
+  })
+      
+
+
+
+--si son items del mod se pone mymod:cosa, si no se pone default:cosa o lo que sea
