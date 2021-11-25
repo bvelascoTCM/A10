256b965234733e28884fe8b96550bd62f959f1a5
AdrianGarciaMartin <77283506+AdrianGarciaMartin@users.noreply.github.com>
2021-11-25 20:39:11 +0100

    ardilla

--- teleport/init.lua
+++ teleport/init.lua
@@ -5,12 +5,18 @@ minetest.register_node("teleport:pad", {
     stack_max = 64,
     groups = {cracky = 3},
     drop = "teleport:pad", --que cuando lo rompas dropee el pad ????
+    
+    on_punch = function (pos, node, player, pointed_thing)
+      local player = minetest.get_player_by_name("singleplayer")
+      
   })
 
 minetest.register_craftitem("teleport:command", {
     description = "Command", 
     inventory_image = "", --completar
   })
+
+
 
 
 
