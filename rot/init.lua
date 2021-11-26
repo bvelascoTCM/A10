8cce4927b261ae16eea6e91d92835ed863c82789
AdrianGarciaMartin <77283506+AdrianGarciaMartin@users.noreply.github.com>
2021-11-26 13:07:13 +0100

    Corrección de algunos errores
    Faltan errores por corregir

--- rot/init.lua
+++ rot/init.lua
@@ -1,27 +1,25 @@
-local c_dirt = minetest.get_content_id("default:dirt")
-local c_rot_dirt = minetest.get_content_id("rot:dirt")
-
 function delete_block_top(pos)
   pos.y = pos.y + 1
   minetest.remove_node(pos)
 end
 
 
-minetest.register_node("rot: dirt", 
+minetest.register_node("rot:dirt",  
   {description = "dirt",
    tiles = "rot_cube.png",
    is_ground_content = true,
-   groups = {cracky = 3},
+   groups = {cracky = 3}
  })
 
-minetest.register_node("rot: purgator", 
+local c_dirt = minetest.get_content_id("default:dirt")
+local c_rot_dirt = minetest.get_content_id("rot:dirt")
+
+minetest.register_node("rot:purgator", 
   {description = "purgator",
    tiles = "Cure_cubo.png", 
    is_ground_content = false, 
    groups = {cracky = 3} 
   })
-
-  
 
 minetest.register_abm({
     nodenames = "default:dirt",
@@ -31,13 +29,15 @@ minetest.register_abm({
     catch_up = false,
     action = function (pos, node, active_object_count, active_object_count_wider)
       local pos = {x = pos.x,  y = pos.y, z = pos.z}
-      minetest.set_node(pos, {name = "rot: dirt"})
+      minetest.set_node(pos, {name = "rot:dirt"})
       delete_block_top(pos)
+    end
+    
   })
 
 minetest.register_abm({
-    nodenames = "rot: dirt",
-    neighbors = "rot: dirt",
+    nodenames = "rot:dirt",
+    neighbors = "rot:dirt",
     interval = 5.0,
     chance = 1,
     catch_up = false,
@@ -45,11 +45,13 @@ minetest.register_abm({
       local pos = {x = pos.x + 1,  y = pos.y, z = pos.z}
       minetest.set_node(pos, {name = "rot: dirt"})
       delete_block_top(pos)
+    end
+    
   })
 
 minetest.register_abm({
-  nodenames = "rot: dirt",
-  neighbors = "rot: dirt",
+  nodenames = "rot:dirt",
+  neighbors = "rot:dirt",
     interval = 20.0,
     chance = 1,
     catch_up = false,
@@ -57,41 +59,44 @@ minetest.register_abm({
       local pos = {x = pos.x,  y = pos.y, z = pos.z}
       minetest.set_node(pos, {name = "default : air"})
       delete_block_top(pos)
+    end
+    
   
 })
 
 minetest.register_abm({
-    nodenames = "rot: purgator",
+    nodenames = "rot:purgator",
     interval = 10.0,
     chance = 1,
     catch_up = false,
-    purge_rotdirt(pos, size) --maybe está mal
-  })
-
-
-local function purge_rotdirt (pos, size)
-  local vm = minetest.get_voxel_manip()
-  local eminx,emaxx = vm:read_from_map(pos.x - size/2, pos.x + size/2) 
-  local eminy, emaxy = vm:read_from_map(pos.y - size/2, pos.y + size/2 )
-  local eminz, emaxz = vm:read_from_map(pos.z - size/2, pos.z + size/2)
-  local a = VoxelArea:new{MinEdge = {eminx, eminy, eminz}, MaxEdge = {emaxx, emaxy, emaxz}} --maybe esta mal
-  local data = vm:get_data()
+    purge_patata = function (pos, size)
+      local vm = minetest.get_voxel_manip()
+      local eminx,emaxx = vm:read_from_map(pos.x - size/2, pos.x + size/2) 
+      local eminy, emaxy = vm:read_from_map(pos.y - size/2, pos.y + size/2 )
+      local eminz, emaxz = vm:read_from_map(pos.z - size/2, pos.z + size/2)
+      local a = VoxelArea:new{MinEdge = {eminx, eminy, eminz}, MaxEdge = {emaxx, emaxy, emaxz}} --maybe esta mal
+      local data = vm:get_data()
   
-  for z = pos.z do
-    for y = pos.y do
-      for x = pos.x do
-        local vi = a:index(x, y, z)
-        if data[vi] == c_rot_dirt then
-          data[vi] = c_dirt
+      for z = eminz,emaxz  do
+        for y = eminy, emaxy do 
+          for x = eminx, emaxx do
+          local vi = a:index(x, y, z)
+          if data[vi] == c_rot_dirt then
+            data[vi] = c_dirt
+          end
         end
       end
     end
-  end
   
-  vm:set_data(data)
-  vm:write_to_map(true)
+    vm:set_data(data)
+    vm:write_to_map(true)
   
-end
+end--maybe está mal
+    
+  })
+
+--82: Required field 'action' of type function
+
 
 
 
